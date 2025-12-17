c2752028ff7ba829f989a48a297bc03d72769c8d
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-12-17 15:57:33 +0100

    add method toDtoList to convert to dto

--- product-service/src/main/java/com/pm/productservice/mapper/ProductMapper.java
+++ product-service/src/main/java/com/pm/productservice/mapper/ProductMapper.java
@@ -5,10 +5,12 @@ import com.pm.productservice.dto.ProductResponseDto;
 import com.pm.productservice.entity.Product;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
     Product toEntity(ProductRequestDto dto);
-
+    List<ProductResponseDto> toDtoList(List<Product> products);
     ProductResponseDto toDto(Product product);
 }
