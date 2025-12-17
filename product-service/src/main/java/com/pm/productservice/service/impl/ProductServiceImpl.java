6dff73da707d643f6352a7c1e9e3b6a3c3e9b967
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-12-17 14:48:00 +0100

    create product service implementation

--- product-service/src/main/java/com/pm/productservice/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/pm/productservice/service/impl/ProductServiceImpl.java
@@ -1,4 +1,36 @@
 package com.pm.productservice.service.impl;
 
-public class ProductServiceImpl {
+import com.pm.productservice.dto.ProductRequestDto;
+import com.pm.productservice.dto.ProductResponseDto;
+import com.pm.productservice.entity.Product;
+import com.pm.productservice.mapper.ProductMapper;
+import com.pm.productservice.repository.ProductRepository;
+import com.pm.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository repository;
+    private final ProductMapper mapper;
+
+    @Override
+    public ProductResponseDto create(ProductRequestDto dto) {
+        Product product = mapper.toEntity(dto);
+        product.setCreatedAt(LocalDateTime.now());
+        return mapper.toDto(repository.save(product));
+    }
+
+    @Override
+    public ProductResponseDto getById(Long id) {
+        Product product = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+        return mapper.toDto(product);
+    }
+
+    // other methods...
 }
